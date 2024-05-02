4dc3738c1cc8bb16abb5448e003326a94ec08e7f
Murilo Wisch <murilowisch1@gmail.com>
2024-05-02 18:08:40 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-git initgit initlet listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -23,7 +23,7 @@ function verificarChute() {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela('p', mensagemTentativas);
+        exibirTextoNaTela('p' , 'mensagemTentativas');
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
